067bed048db9600cbdc7e2912aa4049adba84a36
Fabricio Castillo <46402074+eyrent@users.noreply.github.com>
2022-09-16 11:47:45 -0300

    BaSP-W05-2022: 01_variables_and_operators added + Week 5 folder structure created

---
+++ semana-05/01_variables_and_operators.js
@@ -0,0 +1,22 @@
+/* Crear dos variables numéricas y utilizar el operador suma para guardar
+   el valor de la suma de ambos números en una 3er variable. */
+var numA = 47;
+var numB = 23;
+var AplusB = numA + numB;
+console.log("2.a> AplusB = numA + numB = " + AplusB);
+
+/* Crear dos variables de tipo String y concatenarlas
+   guardando el resultado en una 3er variable. */
+var str1 = "This statement is ";
+var str2 = "made of two parts.";
+var msg = str1 + str2;
+console.log("2.b> String concat is \"" + msg + "\"");
+
+/*Crear dos variables de tipo String y sumar el largo de cada variable
+  (cantidad de letras del string) guardando el resultado de
+  la suma en una 3er variable (utilizar length). */
+var ss1 = "Pineapple pizza is definitely a thing. 🍍";
+var ss2 = "Blue cheese is my second best. 🍕🧀";
+var sumOfLenghts = ss1.length + ss2.length;
+console.log("2.c> sumOfLengths = ", sumOfLenghts);
+
